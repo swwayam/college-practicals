5ff8b322ab31d79811e9b568612c62af2296aafb
unknown <codingswayam24@gmail.com>
2022-03-15 12:00:42 +0530

    JAVA

---
+++ P5/evenNumPrint.java
@@ -0,0 +1,9 @@
+public class evenNumPrint{
+	public static void main(String[] args){
+		for(int i = 1;  i <= 10; i++){
+			if(i%2 == 0){
+				System.out.println(i);
+			}
+		}
+	}
+}
---
+++ P6/doWhilee.java
@@ -0,0 +1,12 @@
+public class WhileEXAMPLE
+{
+	public static void main(String[] args)
+	{
+		int i = 1;
+	while(i<=10)
+	{
+		system.out.println(i);
+		i++;
+	}
+	}
+}
